62d8e5859f9178ebb4307f45e314c845f29768d6
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-12 14:58:58 -0400

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(void) 
+/**
+ * main - Entry point
+ *
+ * Description: 'This program will be using puts to print a sentence'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
 	char str[] = '"Programming is like building a multilingual puzzle';
 
