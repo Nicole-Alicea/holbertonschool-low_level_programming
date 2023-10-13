6daf2b4bd6a1b1052c780cdfadc146cb4c6abde7
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-13 13:50:41 -0400

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,8 +1,17 @@
 #include <unistd.h>
 
+/**
+ * main -Entry point
+ *
+ * Description: 'This program will be using write to print a sentence'
+ *
+ * Return: Always 1 (Success)
+ */
+
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char message[] = "and that piece of art is useful\" - Dora Korpar,\
+			  2015-10-19\n";
 	size_t message_len = sizeof(message) - 1;
 
 	write(1, message, message_len);
