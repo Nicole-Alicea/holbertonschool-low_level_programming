9f62151a998e40d74baa90be03b381fbe0444e5b
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-15 11:51:35 -0400

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -10,10 +10,9 @@
 
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	size_t message_len = sizeof(message) - 1;
+	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, message, message_len);
+	write(2, message, sizeof(message) - 1);
 
 	return (1);
 }
