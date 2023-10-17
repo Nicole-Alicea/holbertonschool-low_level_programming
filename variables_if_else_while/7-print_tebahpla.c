ed2103520f334a1eef5fd33338e6a039150d2884
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-17 13:16:22 -0400

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 0; i < 26; i--)
+{
+putchar(i - '0');
+}
+{
+putchar('\n');
+}
+return (0);
+}
