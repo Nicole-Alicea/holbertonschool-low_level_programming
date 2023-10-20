99ec221d1998965bfb93ff4a19e035866018d342
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-20 14:36:32 -0400

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,14 +7,12 @@
 
 void print_diagonal(int n)
 {
-	int i;
-
-	if (n > 0)
+	if (n <= 0)
 	{
-		for (i = 0; i < n; i++)
-		{
-			_putchar('\\');
-		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+	_putchar('\\');
+	}
 }
