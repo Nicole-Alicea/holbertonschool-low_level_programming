8c8a4a685ef410c2f91afe38221baa711bf8df68
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-20 20:05:07 -0400

    10-triangles

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - This function will print a square followed by a new line
+ * @size: the size of the square
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+}
