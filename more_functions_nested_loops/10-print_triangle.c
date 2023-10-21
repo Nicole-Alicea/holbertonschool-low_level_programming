6cf06717e90a4a8d2906304b04e149ffd9cbc7cb
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-20 20:16:19 -0400

    10-triangles

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -16,10 +16,14 @@ void print_triangle(int size)
         }
         for (i = 0; i < size; i++)
                 {
-                        for (j = 0; j < i; j++)
+                        for (j = 0; j < size - i - 1; j++)
                         {
-                                _putchar('#');
+                                _putchar(' ');
                         }
+			for (j = 0; j < i + 1; j++)
+			{
+				_putchar('#');
+			}
                         _putchar('\n');
                 }
 }
