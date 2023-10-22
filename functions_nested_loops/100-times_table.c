5ed1988811d2783f6b6ce2abe40458b63723a5a2
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-22 15:37:00 -0400

    advanced nested loops

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -8,15 +8,31 @@
 
 void print_times_table(int n)
 {
-	int n;
-
 	if (n > 15 || n < 0)
 	{
-		_putchar(' ');
+		return;
 	}
-	else
+	for (int i = 0; i <= n; i++)
 	{
-		print_times_table();
+		for (int j = 0; j <= n; j++)
+		{
+			int result = i * j;
+			if (j > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (result < 10)
+					_putchar(' ');
+			}
+			if (result < 10)
+				_putchar(' ');
+			if (result < 100)
+				_putchar(' ');
+			_putchar((result / 100) + '0');
+			_putchar((result / 10) % 10 + '0');
+			_putchar((result % 10) + '0');
+		}
 		_putchar('\n');
 	}
 }		
