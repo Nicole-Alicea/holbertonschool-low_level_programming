c456db9cd11c281972aeaab82774e4eb4b02ad82
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-21 22:42:42 -0400

    advanced nested loops

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, m, p, q;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = n + 1; m <= 9; m++)
+		{
+			for (p = 0; p <= 9; p++)
+			{
+				for (q = p + 1; q <= 9; q++)
+				{
+					putchar(n + '0');
+					putchar(p + '0');
+					putchar(' ');
+					putchar(m + '0');
+					putchar(q + '0');
+					
+					if (n != 9 || m != 9 || p != 8 || q != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
