c746485f83b8da9db7bb3a506e4d10fc1bc4b087
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-27 19:28:24 -0400

    task 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -8,3 +8,26 @@
 
 void print_array(int *a, int n)
 {
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
+
+int main()
+{
+	int arr[] = {1, 2, 3, 4, 5};
+	int n = 5;
+
+	printf("Array elements: ");
+	print_array(arr, n);
+
+	return (0);
+}
