38cdfd9902f16e87cad2b00756d70475ef10312e
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-26 22:34:12 -0400

    pointers

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,9 +2,19 @@
 
 /**
  * puts2 - Prints every other character of a string starting with the first
+ * @str: String to be printed
  *
  * Return: void
  */
 
 void puts2(char *str)
 {
+	char str;
+	int len = strlen(str);
+
+	for (str = 0; str < len; str += 2)
+	{
+		puts2(str);
+	}
+	putchar('\n');
+}
