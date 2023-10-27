32fc1c7b4083df685b08524f36edf4ccaf407121
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-27 19:07:03 -0400

    task 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,3 +8,22 @@
 
 void puts_half(char *str)
 {
+	int length = strlen(str);
+	int start_index;
+
+	if(length % 2 == 0)
+	{
+		start_index = length / 2;
+	}
+	else
+	{
+		start_index = (length - 1) / 2;
+	}
+	int i;
+
+	for (i = start_index; i < length; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
