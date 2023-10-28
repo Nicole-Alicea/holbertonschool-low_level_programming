359132247aa5cf5800042630fbe295a9e41e2304
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-27 20:33:01 -0400

    task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdbool.h>
 
 /**
  * _atoi - Converts a string to an integer
@@ -8,3 +9,43 @@
 
 int _atoi(char *s)
 {
+	int result = 0;
+	int sign = 1;
+	bool foundDigit = false;
+
+	while (*s)
+	{
+		if (*s == ' ')
+		{
+			s++;
+			continue;
+		}
+		if (*s == '-')
+		{
+			sign = -1;
+			s++;
+		}
+		else if (*s == '+')
+		{
+			s++;
+		}
+		while (*s >= '0' && *s <= '9')
+		{
+			foundDigit = true;
+			int digit = *s - '0';
+
+			if (result > (INT_MAX - digit) / 10)
+			{
+				return sign == 1 ? INT_MAX : INT_MIN;
+			}
+			result = result * 10 + digit;
+			s++;
+		}
+		if (!foundDigit)
+		{
+			return (0);
+		}
+		return result * sign;
+	}
+	return (0);
+}
