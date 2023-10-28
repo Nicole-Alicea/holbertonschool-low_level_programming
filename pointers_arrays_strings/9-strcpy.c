61a5a64ae784d0978e6a0d485bbbff8378b53dbc
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-27 20:13:53 -0400

    task 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -8,3 +8,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
+	char *original_dest = dest;
+
+	while (*src != '\0')
+	{
+		*dest - *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return original_dest;
+}
