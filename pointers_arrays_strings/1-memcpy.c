629ee30382f9cf1d8581f4252fe8ecdc6e2f9c49
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-29 16:30:42 -0400

    task 1 3rd project

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - This function copies memory area
+ * @n: the amount of bytes copied
+ * @src: source memory area
+ * @dest: destination memory area
+ *
+ * Return: pointer to destination
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *dest_start = dest;
+	while (n--)
+	{
+		*dest++ = *src++;
+	}
+	return dest_start;
+}
