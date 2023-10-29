85111d4b46e31f7ed861b00522cf1f551bba3776
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-29 17:14:51 -0400

    task 6 3rd project

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - This function prints the chessboard
+ * @row: rows of the chessboard
+ * @col: collumns of the chessboard
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			printf("%c", a[row][col]);
+		}
+		printf("\n");
+	}
+}
