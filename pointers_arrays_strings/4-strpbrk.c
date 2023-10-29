7def042ba2ad2539486680e9590b58036a2ee00a
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-29 16:46:37 -0400

    task 4 3rd project

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -7,7 +7,8 @@
  * @s: string that will be searched
  * @accept: string containing the bytes
  *
- * Return: pointer to the byte in s that matches one of the bytes in accept, or NULL if no such byte is found
+ * Return: pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found
  */
 
 char *_strpbrk(char *s, char *accept)
