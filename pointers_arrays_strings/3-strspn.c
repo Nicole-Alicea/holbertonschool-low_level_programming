54ceac37c7c6d97c2b28d82cdd88b5c94af83b5a
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-28 22:16:15 -0400

    task 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,11 +10,12 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int count = 0;
 	int found = 0;
+	char *a;
 
 	while (*s)
 	{
 		found = 0;
-		for (char *a = accept; *a; a++)
+		for (*a = accept; *a; a++)
 		{
 			if (*s == *a)
 			{
