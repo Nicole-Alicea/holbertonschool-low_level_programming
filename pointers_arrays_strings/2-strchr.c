2e7bf00ee083814a78b66339b19e4b5c0e61de88
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-29 08:28:48 -0400

    task 2wq project 3

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+#include <string.h>
+
+/**
+ *
+ *
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	for (i = 0; (s + i) != '\0'; i++)
+	{
+		if ((s + i) == c)
+		{
+			return (s + i);
+		}
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
