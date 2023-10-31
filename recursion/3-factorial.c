08e0c26aec539fa370e4da33def4041bf932ab50
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-31 19:39:16 -0400

    recursion

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -13,6 +13,19 @@ int factorial(int n)
 	{
 		return (-1);
 	}
-	n * factorial(n - 1);
-	return (n * factorial(n - 1));
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		int result = 1;
+		int i;
+
+		for (i = 1; i <=n; i++)
+		{
+			result *= i;
+		}
+		return (result);
+	}
 }
