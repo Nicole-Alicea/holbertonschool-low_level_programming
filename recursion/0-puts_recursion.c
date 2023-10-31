2a302346516b6b0b564e30cbeab1f2a21e889abe
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-31 13:30:46 -0400

    First recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - This function will print a string followed by a new line
+ * @s: string that will be printed
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
