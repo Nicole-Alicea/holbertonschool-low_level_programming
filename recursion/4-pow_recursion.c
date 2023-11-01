dc58d03c1b635af6743d521a5546b7979b318d79
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-31 20:03:09 -0400

    recursion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -14,9 +14,13 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
+	else if (y == 0)
+	{
+		return (1);
+	}
 	else
 	{
-		int result = (_pow_recursion(x, y));
-		return (result);
+		_pow_recursion(x, y);
+		return (_pow_recursion(x, y));
 	}
 }
