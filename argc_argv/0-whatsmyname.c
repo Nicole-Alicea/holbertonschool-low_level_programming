691d874ba2f95ef47e61831fa87f177758ba3f49
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-31 22:18:30 -0400

    argc

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argv: name of the program
+ * @argc: array
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	else
+	{
+		printf("Unable to determine program name\n");
+	}
+	return (0);
+}
