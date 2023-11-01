88bf8c592623fc5626a87d639404b39f2b653d93
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-01 15:43:49 -0400

    recursion

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -8,32 +8,24 @@
  * Return: 1 if the integer is a prime number, otherwise 0
  */
 
-int is_prime_number(int n)
+int is_prime_recursive(int n, int i)
 {
-	return (helper_prime(n, 2, n / 2));
-}
-
-/**
- * helper_prime - blank
- * @n: check1
- * @i: will be used to increment
- * @limit: will be used to stop
- *
- * Return: 0
- */
-
-int helper_prime(int n, int i, int limit)
-{
-	if ((n % i == 0 && i <= limit) || n < 0 || n == 1)
+	if (n <= 1)
 	{
 		return (0);
 	}
-	else if (n % i != 0 && i <= limit)
-	{
-		return (helper_prime(n, i + 1, limit));
-	}
-	else
+	if (i == 1)
 	{
 		return (1);
 	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (is_prime_recursive(n, i - 1));
+}
+
+int is_prime_number(int n)
+{
+	return (is_prime_recursive(n, n - 1));
 }
