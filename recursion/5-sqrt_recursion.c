1863cc4c6d7bd04981fb962a845e25915f7f9f00
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-10-31 21:05:39 -0400

    recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -13,13 +13,13 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	else if (n == 0 || n ==1))
+	else if (n == 0 || n ==1)
 	{
 		return (n);
 	}
 	else
 	{
 		int result = _sqrt_recursion(n - 1);
-		return (result * result <= n ? result : result - 1);
+		return (result * result <= n ? result : result - 1;)
 	}
 }
