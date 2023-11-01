b99df13c67acb4344bcba40b6bb4f2d1452c244e
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-01 13:33:08 -0400

    argc and argv

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,6 +12,7 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2, result;
+	char *endptr;
 
 	if (argc != 3)
 	{
@@ -19,7 +20,17 @@ int main(int argc, char *argv[])
 
 		return (1);
 	}
-	if (sscanf(argv[1], "%d", &num1) != 1 || sscanf(argv[2], "%d", &num2) != 1)
+	num1 = strtol(argv[1], &endptr, 10);
+
+	if (*endptr != '\0')
+	{
+		printf("Error: Invalid input. Please provide two valid integers.\n");
+
+		return (1);
+	}
+	num2 = strtol(argv[2], &endptr, 10);
+
+	if (*endptr != '\0')
 	{
 		printf("Error: Invalid input. Please provide two valid integers.\n");
 
