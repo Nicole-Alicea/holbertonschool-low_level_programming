cc33c36578f194bdd9f51d6bb30e2b2ca0aa7816
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-05 20:15:04 -0400

    advanced task argc

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -25,13 +25,15 @@ int minCoins(int cents)
 
 int main(int argc, char *argv[])
 {
+	int cents;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 
 		return (1);
 	}
-	int cents = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
 	if (cents < 0)
 	{
