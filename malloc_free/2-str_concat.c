0da44aeefab0e72ceed2f7a045cc6d7c5f70d27f
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-06 16:11:39 -0400

    malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,8 +17,10 @@ char *str_concat(char *s1, char *s2)
 	size_t len1, len2, total_len;
 	char *result;
 
-	if (s1 == NULL) s1 = "";
-	if (s2 == NULL) s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
