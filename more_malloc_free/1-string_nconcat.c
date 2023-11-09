f4210ca2b0f19301c04b953fbb4a8dfd42d02c88
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-09 13:27:34 -0400

    more malloc

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -25,24 +25,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
-	total_len = len1 + len2;
+	total_len = len1 + n;
 
-	ptr = (char *)malloc(sizeof(char) * total_len + 1);
+	s3 = (char *)malloc(sizeof(char) * (total_len + 1));
 
-	s3 = s1 + s2;
+	if (s3 == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(s3, s1);
 
 	if (n >= len2)
 	{
-		strcpy(s3, s1);
 		strcat(s3, s2);
-
-		return (s3);
 	}
-	if (n < len2)
+	else
 	{
-		strcpy(s3, s1);
 		strncat(s3, s2, n);
-
-		return (s3);
 	}
+	
+	return (s3);
 }
