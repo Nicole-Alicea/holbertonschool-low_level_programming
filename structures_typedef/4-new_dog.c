758a2549d4e3efcff9e70218d7f4f69d601b666e
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-14 13:34:16 -0400

    structures

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,36 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: new dog or NULL if fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog = (dog_t *)malloc(sizeof(dog_t));
+
+	if (newDog == NULL)
+	{
+		return (NULL);
+	}
+
+	newDog->name = strdup(name);
+	newDog->owner = strdup(owner);
+
+	if (newDog->name == NULL || newDog->owner == NULL)
+	{
+		free(newDog->name);
+		free(newDog->owner);
+		free(newDog);
+
+		return (NULL);
+	}
+
+	newDog->age = age;
+
+	return (newDog);
+}
