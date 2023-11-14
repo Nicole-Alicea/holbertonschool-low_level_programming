42ca41df90c47941fd9a69404a05d7614c0da9f5
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-14 14:32:21 -0400

    structures

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stddef.h>
 
 /**
  * free_dog - function that frees dogs
@@ -11,8 +10,9 @@
 
 void free_dog(dog_t *d)
 {
-	if (d != NULL)
+	if (d)
 	{
+		free(d->owner);
 		free(d->name);
 		free(d);
 	}
