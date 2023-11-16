b5cbddeb27124f96a103b98f74650c37428d0e26
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-15 23:34:58 -0400

    function pointers

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -10,8 +10,6 @@
 
 int op_add(int a, int b)
 {
-	op_t;
-
 	return (a + b);
 }
 
@@ -25,8 +23,6 @@ int op_add(int a, int b)
 
 int op_sub(int a, int b)
 {
-	op_t;
-
 	return (a - b);
 }
 
@@ -40,8 +36,6 @@ int op_sub(int a, int b)
 
 int op_mul(int a, int b)
 {
-	op_t;
-
 	return (a * b);
 }
 
@@ -55,8 +49,6 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-	op_t;
-
 	return (a / b);
 }
 
@@ -70,7 +62,5 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-	op_t;
-
 	return (a % b);
 }
