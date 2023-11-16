f6ed5973676bd8cda0a5c40641462593b13baf6f
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-15 20:34:49 -0400

    function pointers

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: name to be printed
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
