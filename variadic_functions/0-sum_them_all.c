503815f1a0335e228281bc984a6ee7a09d20d003
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-16 21:01:47 -0400

    variadic functions

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - adds all of its parameters
+ * @n: number to be added
+ *
+ * Return: sum of all its parameters or 0 if n == 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int i, sum;
+
+	va_start (ap, n);
+
+	sum = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n == 0)
+		{
+			return (0);
+		}
+		sum += va_arg (ap, int);
+	}
+	va_end (ap);
+
+	return (sum);
+}
---
+++ variadic_functions/README.md
@@ -0,0 +1,1 @@
+This directory contains files pertaining to variadic functions.
