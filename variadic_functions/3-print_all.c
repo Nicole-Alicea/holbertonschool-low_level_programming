fcaf7f6154acda984c404a9c49be6bd91ec40fb5
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-19 19:49:28 -0400

    fixing Betty

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -19,17 +19,18 @@ void print_all(const char * const format, ...)
 
 	while (format && format[i])
 	{
-		if ((format[i] == 'c' || format[i] == 'i' ||
-					format[i] == 'f' || format[i] == 's') && (((format[i] == 'c')
-					? (printf("%c", va_arg(args, int)), 1) :
-					(format[i] == 'i') ? (printf("%d", va_arg(args, int)), 1) :
-					(format[i] == 'f') ? (printf("%f", va_arg(args, double)), 1) :
-					((format[i] == 's') ? ((str = va_arg(args, char *))
-					!= NULL ? (printf("%s", str), 1) :
-					(printf("(nil)"), 1)) : 0)) && format[i + 1] != '\0'))
-				{
-					printf(", ");
-				}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+			format[i] == 's') && ((format[i] == 'c') ?
+			(printf("%c", va_arg(args, int)), 1) :
+			(format[i] == 'i') ? (printf("%d", va_arg(args, int)), 1) :
+			(format[i] == 'f') ? (printf("%f", va_arg(args, double)), 1) :
+			(format[i] == 's') ? ((str = va_arg(args, char *))
+			!= NULL ? (printf("%s", str), 1) :
+					(printf("(nil)"), 1) : 0) &&
+			format[i + 1] != '\0'))
+		{
+			printf(", ");
+		}
 		i++;
 	}
 	printf("\n");
