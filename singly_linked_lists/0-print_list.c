4255a7ff646cc4a6c8ae021eae208a800cea40ea
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-30 16:33:15 -0400

    fixing betty style task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,12 +13,15 @@ size_t print_list(const list_t *h)
 
 	for (n = 0; h != NULL; n++)
 	{
-	if (h->str == NULL)
-		printf("[0] (nil)\n");
-
-	else
-		printf("[%d] %s\n", h->len, h->str);
-	h = h->next;
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
 	}
 	return (n);
 }
