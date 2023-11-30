f4d504c45843df2eb3f67cf81f560a1cbc4ca355
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-29 21:22:30 -0400

    singly linked lists

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,16 +11,19 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	if (h->str == NULL)
+	while (h != NULL)
 	{
-		printf("[0] (nil)\n");
-	}
-	else
-	{
-		printf("[%u] %s\n", h->len, h->str);
-
-		h = h->next;
-		count++;
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			
+			h = h->next;
+			count++;
+		}
 	}
 	return (count);
 }
