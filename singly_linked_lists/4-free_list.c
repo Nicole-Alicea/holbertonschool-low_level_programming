3d3e0567b4f4e9a90eec2dc64a6f60cdb4e38d90
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-30 17:06:12 -0400

    task 4 singly linked lists

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - This function frees a list_t list
+ * @head: pointer to head
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next ;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
