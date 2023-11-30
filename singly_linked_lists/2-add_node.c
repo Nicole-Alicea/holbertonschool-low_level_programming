073f435a8304d9224856eddf06651f2bcd2d9de4
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-30 16:41:51 -0400

    fixing task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,13 +12,12 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
-	if (str == NULL)
-		return (NULL);
-
-	*new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
+	{
 		return (NULL);
+	}
 
 	new_node->str = strdup(str);
 
@@ -27,6 +26,8 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
+
+	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
