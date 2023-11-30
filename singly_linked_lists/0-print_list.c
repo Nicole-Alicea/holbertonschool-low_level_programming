06bb819aad2da8558f5094db09657c56133d0ea1
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-29 21:09:59 -0400

    singly linked lists

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -7,7 +7,20 @@
  * Return: The number of nodes
  */
 
-typedef list_t;
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h)
 {
@@ -22,7 +35,7 @@ size_t print_list(const list_t *h)
 		printf("[%u] %s\n", h->len, h->str);
 
 		h = h->next;
-		count++
+		count++;
 	}
 	return (count);
 }
