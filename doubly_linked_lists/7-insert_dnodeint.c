61fad4f16618cea54ddcb9f03ac1363df1a58ad4
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-12-01 10:36:10 -0400

    doubly linked lists task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,32 +10,30 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp, *new_node;
+	dlistint_t *new_node = NULL;
+
+	if (h == NULL)
+		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	if (idx > 1 && *h != NULL)
+		return (insert_dnodeint_at_index(&((*h)->next), idx - 1, n));
+	if (idx > 1 || *h == NULL)
+		return (NULL);
+	if ((*h)->next == NULL)
+		return (add_dnodeint_end(h, n));
+
 	new_node = malloc(sizeof(dlistint_t));
 
-	if(!new)
+	if (node == NULL)
+	{
+		dprintf(2, "Error\n");
 		return (NULL);
-	new_node-> = n;
-	temp = *h;
-	if (!temp)
-		new_node->prev = NULL;
-		new_node->next = NULL;
-		*h = new_node;
-		return (new_node);
-	if (idx == 0)
-		new_node->prev = NULL;
-		new_node->next = temp;
-		temp->prev = new_node;
-		*h = new_node;
-		return (new_node);
-	for ( ; idx > 1 && temp->next; idx--)
-		temp = temp->next;
-	if (idx > 1 && !temp->next)
-		return (NULL);
-	new_node->prev = temp;
-	new_node->next = temp->next ? temp->next : NULL;
-	if (temp->next)
-		temp->next->prev = new_node;
-	temp->next = temp->next && idx > 1 ? NULL : new_node;
+	}
+	new_node->n = n;
+	new_node->next = (*h)->next;
+	new_node->prev = (*h);
+	(*h)->next->prev = new_node;
+	(*h)->next = new_node;
 	return (new_node);
 }
