dec98201d645a59c7effaa5da355091d7947b2bb
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-11-30 22:18:49 -0400

    doubly linked lists task 1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - This function returns the number of elements in a
+ * linked dlistint_t list
+ * @h: Pointer to head
+ *
+ * Return: Number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t m = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	while (h->prev != NULL)
+	{
+		h = h->prev;
+	}
+	
+	while (h != NULL)
+	{
+		m++;
+		h = h->next;
+	}
+
+	return (m);
+}
