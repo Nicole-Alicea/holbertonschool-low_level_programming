a78e908fb6c1d4eea72c3553cf096a85ac31a011
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-12-08 15:20:03 -0400

    task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,45 +12,20 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new;
-	char *value_copy;
-	unsigned long int index n;
+	hash_node_t *new_node;
+	unsigned int index = hash_function(key);
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 
-	value_copy = strdup(value);
+	new_node = create_node(key, value);
 
-	if (value_copy == NULL)
-		return (0);
-
-	index = key_index((const unsigned char *)key, ht->size);
-
-	for (n = index; ht->array[n]; n++)
+	if (new_node == NULL)
 	{
-		if (strcmp(ht->array[n]->key, key) == 0)
-		{
-			free(ht->array[n]->value);
-			ht->array[n]->value = value_copy;
-			return (1);
-		}
-	}
-	new = malloc(sizeof(hash_node_t));
-
-	if (new == NULL)
-	{
-		free(value_copy);
 		return (0);
 	}
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(new);
-		return (0);
-	}
-	new->value = value_copy;
-	new->next = ht->array[index];
-	ht->array[index] = new;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
 	return (1);
 }
