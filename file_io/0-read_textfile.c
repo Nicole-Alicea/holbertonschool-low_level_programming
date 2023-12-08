04dea47ff6eef6cf9a91a40b84a6fd42489da642
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-12-08 11:30:14 -0400

    task 0 file i/0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * read_textfile - This function reads a text file and
+ * prints it to the POSIX standard output
+ * @filename: Pointer to the file containing the letters to be read
+ * @letters: The number of letters it should read and print
+ *
+ * Return: Actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;/* fd stands for file descriptor */
+	char *buffer;
+	ssize_t bytes_read, bytes_written;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buffer = (char *)malloc(letters);
+	
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	bytes_read = read(fd, buffer, letters);
+
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	free(buffer);
+	close(fd);
+	return (bytes_written);
+}
