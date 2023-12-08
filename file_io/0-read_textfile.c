78f83c17de1b0d412097337cf8a5e1213774ad37
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-12-08 11:42:50 -0400

    fixing task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -36,22 +36,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	bytes_read = read(fd, buffer, letters);
-
-	if (bytes_read == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
 	free(buffer);
 	close(fd);
 	return (bytes_written);
