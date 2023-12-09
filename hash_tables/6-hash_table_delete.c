a51a51ef260ba9fc2aee98279f06c0aa2ca89469
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-12-09 18:44:58 -0400

    task 6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - This function deletes a hash table
+ * @ht: The hash table to be deleted
+ *
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	size_t n;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+
+	for (n = 0; n < ht->size; n++)
+	{
+		node* current = ht->array[n];
+		node* next;
+
+		while (current != NULL)
+		{
+			next = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = next;
+		}
+	}
+
+	free(ht->array);
+
+	free(ht);
+}
