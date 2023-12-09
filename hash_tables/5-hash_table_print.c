1a8e051422a34033b304a03aad9501efbe1a8c34
Nicole-Alicea <na.aliceanicole@gmail.com>
2023-12-09 13:09:33 -0400

    task 5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -26,12 +26,11 @@ void hash_table_print(const hash_table_t *ht)
 		while (temp != NULL)
 		{
 			if (flag == 1)
-			{
+			
 				printf(", ");
 				printf("'%s': '%s'", temp->key, temp->value);
 				flag = 1;
 				temp = temp->next;
-			}
 		}
 	}
 	printf("}\n");
